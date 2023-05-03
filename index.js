33ea5a0d1e80dc4a06e6e499274f463aa3ef6e75
finless88 <nazmul96@gmail.com>
2023-05-04 03:41:41 +0600

    vercel is configure and sever side is deployed to vercel

--- index.js
+++ index.js
@@ -25,9 +25,20 @@ app.get("/chef", (req, res) => {
   res.send(allchef);
 });
 
+// load specific chef description..just one unique chef so use find()
+// beacuse find automatically return only one unique
 app.get("/chef/:id", (req, res) => {
+  const cid = parseInt(req.params.id);
+  console.log("cid", cid);
+  const selectedChef = allchef.find((item) => parseInt(item.id) === cid);
+  // console.log(selectedChef);
+  res.send(selectedChef);
+});
+
+// load all recipe of specific chef
+app.get("/viewrecipe/:id", (req, res) => {
   const chefId = parseInt(req.params.id);
-  console.log("chefId", chefId);
+  //   console.log("chefId", chefId);
   //   as we get the id we will run filter() on recipes to get those recipe od that chefid provided
 
   const selectedChefRecipe = recipes.filter(
@@ -37,9 +48,6 @@ app.get("/chef/:id", (req, res) => {
   res.send(selectedChefRecipe);
 });
 
-app.get("/viewrecipes", (req, res) => {
-  res.send(recipes);
-});
 app.listen(port, () => {
   console.log(`Example app listening on port ${port}`);
 });
